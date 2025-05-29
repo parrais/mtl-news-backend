3e7a9e39f5c4d98a94ccfb5043bb9a9fdd1c0b24
parrais <github@parrais.co.uk>
2025-05-29 18:38:54 +0100

    Completing task 5: Entity Relationship Diagram (created in dbdiagram.io) and minor edits to table creation (e.g. not null)

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -16,23 +16,23 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
     })
     .then(() => {
       return db.query(`CREATE TABLE topics(
-      slug VARCHAR(20) PRIMARY KEY,
+      slug VARCHAR(20) PRIMARY KEY NOT NULL,
       description VARCHAR(100),
       img_url VARCHAR(1000));`);
     })
     .then(() => {
       return db.query(`CREATE TABLE users(
-      username VARCHAR(20) PRIMARY KEY,
+      username VARCHAR(20) PRIMARY KEY NOT NULL,
       name VARCHAR(100),
       avatar_url VARCHAR(1000));`);
     })
     .then(() => {
       return db.query(`CREATE TABLE articles(
       article_id SERIAL PRIMARY KEY,
-      title VARCHAR(200),
+      title VARCHAR(200) NOT NULL,
       topic VARCHAR(20) REFERENCES topics(slug),
       author VARCHAR(20) REFERENCES users(username),
-      body TEXT,
+      body TEXT NOT NULL,
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       votes INT DEFAULT 0,
       article_img_url VARCHAR(1000));`);
@@ -40,10 +40,10 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
     .then(() => {
       return db.query(`CREATE TABLE comments(
       comment_id SERIAL PRIMARY KEY,
-      article_id INT REFERENCES articles(article_id),
-      body TEXT,
+      article_id INT NOT NULL REFERENCES articles(article_id),
+      body TEXT NOT NULL,
       votes INT DEFAULT 0,
-      author VARCHAR(20) REFERENCES users(username),
+      author VARCHAR(20) NOT NULL REFERENCES users(username),
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);`);
     })
     .then(() => {
