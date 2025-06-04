883aa3f512987a5b8368a785990e23c15c7e257b
Matt Lewis <50201339+parrais@users.noreply.github.com>
2025-06-04 12:54:33 +0100

    Simplify API documentation endpoint

--- models/api.models.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs/promises");
-
-const fetchEndpoints = () => {
-  return fs.readFile("./endpoints.json", "utf-8").then((endpointInfo) => {
-    return JSON.parse(endpointInfo);
-  });
-};
-
-module.exports = { fetchEndpoints };
