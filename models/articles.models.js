6df13ef5c5b416b3d1ff1b75e6c9a6c538704985
parrais <github@parrais.co.uk>
2025-08-26 16:20:51 +0100

    Correct typo in variable

--- models/articles.models.js
+++ models/articles.models.js
@@ -1,55 +1,55 @@
-const db = require("../db/connection");
-const format = require("pg-format");
+const db = require('../db/connection');
+const format = require('pg-format');
 
 const fetchArticles = ({ sort_by, order, topic }) => {
-  const DEFAULT_SORT_COLUMN = "created_at";
-  const DEFAULT_ORDER = "desc";
-  if (sort_by === undefined || sort_by === "") {
+  const DEFAULT_SORT_COLUMN = 'created_at';
+  const DEFAULT_ORDER = 'desc';
+  if (sort_by === undefined || sort_by === '') {
     sort_by = DEFAULT_SORT_COLUMN;
   }
-  if (order === undefined || order === "") {
+  if (order === undefined || order === '') {
     order = DEFAULT_ORDER;
   }
   const allowedSortColumns = [
-    "author",
-    "title",
-    "article_id",
-    "topic",
-    "created_at",
-    "votes",
-    "article_img_url",
-    "comment_count",
+    'author',
+    'title',
+    'article_id',
+    'topic',
+    'created_at',
+    'votes',
+    'article_img_url',
+    'comment_count',
   ];
-  const allowedOrders = ["asc", "desc"];
+  const allowedOrders = ['asc', 'desc'];
   if (!allowedSortColumns.includes(sort_by) || !allowedOrders.includes(order)) {
-    return Promise.reject({ status: 400, msg: "Invalid Input" });
+    return Promise.reject({ status: 400, msg: 'Invalid Input' });
   }
 
   const queryParams = [];
   let queryString =
-    "SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count FROM articles LEFT OUTER JOIN comments ON articles.article_id = comments.article_id ";
+    'SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count FROM articles LEFT OUTER JOIN comments ON articles.article_id = comments.article_id ';
 
   if (topic) {
     queryParams.push(topic);
     queryString += `WHERE topic = $${queryParams.length} `;
   }
 
-  if (sort_by === "comment_count") {
+  if (sort_by === 'comment_count') {
     queryString +=
-      "GROUP BY articles.article_id ORDER BY COUNT(comments.comment_id)";
+      'GROUP BY articles.article_id ORDER BY COUNT(comments.comment_id)';
   } else {
-    queryString += "GROUP BY articles.article_id ORDER BY articles.%I";
+    queryString += 'GROUP BY articles.article_id ORDER BY articles.%I';
   }
 
-  if (order === "asc") {
-    queryString += " ASC;";
+  if (order === 'asc') {
+    queryString += ' ASC;';
   } else {
-    queryString += " DESC;";
+    queryString += ' DESC;';
   }
 
-  QueryString = format(queryString, sort_by);
+  const formattedQueryString = format(queryString, sort_by);
 
-  return db.query(QueryString, queryParams).then(({ rows }) => {
+  return db.query(formattedQueryString, queryParams).then(({ rows }) => {
     const articles = rows.map((article) => {
       const correctedArticle = { ...article };
       correctedArticle.comment_count = Number(article.comment_count);
@@ -62,7 +62,7 @@ const fetchArticles = ({ sort_by, order, topic }) => {
 const fetchArticleById = (article_id) => {
   return db
     .query(
-      "SELECT articles.author, articles.title, articles.article_id, articles.body, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count FROM articles LEFT OUTER JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id",
+      'SELECT articles.author, articles.title, articles.article_id, articles.body, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count FROM articles LEFT OUTER JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id',
       [article_id]
     )
     .then(({ rows }) => {
@@ -84,7 +84,7 @@ const changeArticleVotes = (article_id, inc_votes) => {
   }
   return db
     .query(
-      "UPDATE articles SET votes = votes + $2 WHERE article_id = $1 RETURNING *",
+      'UPDATE articles SET votes = votes + $2 WHERE article_id = $1 RETURNING *',
       [article_id, inc_votes]
     )
     .then(({ rows }) => {
@@ -101,23 +101,23 @@ const changeArticleVotes = (article_id, inc_votes) => {
 
 const insertArticle = (author, title, body, topic, article_img_url) => {
   if (
-    typeof title !== "string" ||
-    title === "" ||
-    typeof author !== "string" ||
-    author === "" ||
-    typeof topic !== "string" ||
-    topic === "" ||
-    typeof body !== "string" ||
-    body === ""
+    typeof title !== 'string' ||
+    title === '' ||
+    typeof author !== 'string' ||
+    author === '' ||
+    typeof topic !== 'string' ||
+    topic === '' ||
+    typeof body !== 'string' ||
+    body === ''
   ) {
     return Promise.reject({
       status: 400,
       msg: `Invalid input`,
     });
   }
-  if (!article_img_url || typeof article_img_url !== "string") {
+  if (!article_img_url || typeof article_img_url !== 'string') {
     article_img_url =
-      "https://upload.wikimedia.org/wikipedia/commons/1/1d/Breaking_News-Alert.png";
+      'https://upload.wikimedia.org/wikipedia/commons/1/1d/Breaking_News-Alert.png';
   }
   return db
     .query(
@@ -127,7 +127,7 @@ const insertArticle = (author, title, body, topic, article_img_url) => {
     .then(({ rows }) => {
       const newArticleId = rows[0].article_id;
       return db.query(
-        "SELECT articles.author, articles.title, articles.article_id, articles.body, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count FROM articles LEFT OUTER JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id",
+        'SELECT articles.author, articles.title, articles.article_id, articles.body, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count FROM articles LEFT OUTER JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id',
         [newArticleId]
       );
     })
